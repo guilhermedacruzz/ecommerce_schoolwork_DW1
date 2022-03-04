2f5efbd6c955490983672efbe7a28f7bac7a9fc0
indexGuilherme <guidacruz2004@gmail.com>
2022-03-04 16:46:03 -0300

    conseguindo deixar o carousel responsivo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,12 +9,12 @@
     <div id="slide-menu" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <div class="d-flex justify-content-evenly flex-wrap-reverse p-md-5 p-3">
+                <div class="d-flex justify-content-evenly align-items-center flex-wrap-reverse p-md-5 p-3 h-100">
                     <div class="d-flex flex-column align-items-start justify-content-center item-content">
                         <div class="border-start border-5 ps-5">
                             <h1>Lorem, ipsum dolor.</h1>
                             <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-                        </div>
+                        </div>  
                         <h6 class="ms-5">3.78$</h6>
                         <button class="btn btn-dark ms-5">Lorem.</button>
                     </div>
@@ -24,7 +24,7 @@
                 </div>
             </div>
             <div class="carousel-item">
-                <div class="d-flex justify-content-evenly flex-wrap-reverse p-md-5 p-3">
+                <div class="d-flex justify-content-evenly align-items-center flex-wrap-reverse p-md-5 p-3 h-100">
                     <div class="d-flex flex-column align-items-start justify-content-center item-content">
                         <div class="border-start border-5 ps-5">
                             <h1>Lorem, ipsum dolor.</h1>
@@ -34,12 +34,12 @@
                         <button class="btn btn-dark ms-5">Lorem.</button>
                     </div>
                     <div class="d-flex flex-column">    
-                        <img class="item-img" src="img/controller.png">
+                        <img class="item-img" src="img/keyboard.png">
                     </div>
                 </div>
             </div>
             <div class="carousel-item">
-                <div class="d-flex justify-content-evenly flex-wrap-reverse p-md-5 p-3">
+                <div class="d-flex justify-content-evenly align-items-center flex-wrap-reverse p-md-5 p-3 h-100">
                     <div class="d-flex flex-column align-items-start justify-content-center item-content">
                         <div class="border-start border-5 ps-5">
                             <h1>Lorem, ipsum dolor.</h1>
@@ -49,7 +49,7 @@
                         <button class="btn btn-dark ms-5">Lorem.</button>
                     </div>
                     <div class="d-flex flex-column">    
-                        <img class="item-img" src="img/controller.png">
+                        <img class="item-img" src="img/mouse.png">
                     </div>
                 </div>
             </div>
